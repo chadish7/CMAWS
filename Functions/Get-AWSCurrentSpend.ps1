e2546918eb14e05138e5983779f8aac2bb501043
Chad Miles <chadish7@gmail.com>
2021-03-12 15:26:53 -0600

    Added Last Month spend to Get-AWSCurrentSpend

--- Functions/Get-AWSCurrentSpend.ps1
+++ Functions/Get-AWSCurrentSpend.ps1
@@ -1,15 +1,20 @@
 Function Get-AWSCurrentSpend {
-    $SpendPeriod = @{
+    $CurrentPeriod = @{
         Start = Get-Date -UFormat "%Y-%m-%d" -Day 1 
         End   = Get-Date -UFormat "%Y-%m-%d"
+    }
+    $LastMonth = @{
+        Start = Get-Date -UFormat "%Y-%m-%d" -Day 1 -Month (Get-Date).AddMonths(-1).Month
+        End   = Get-Date -UFormat "%Y-%m-%d" -Day 1
     }
     $FCPeriod = @{
         Start = Get-Date -Day (Get-Date).AddDays(1).Day -UFormat "%Y-%m-%d"
         End   = Get-Date -Day (Get-Date -Month (Get-Date).AddMonths(1).Month -Day 1).AddDays(-1).Day -UFormat "%Y-%m-%d"
     }
     $Output = @{
-        Date     = $SpendPeriod.End
-        Spend    = [Math]::Round((Get-CECostAndUsage -TimePeriod $SpendPeriod -Granularity Monthly -Metric UNBLENDED_COST).ResultsByTime.Total.Values.Amount,2)
+        Date           = $CurrentPeriod.End
+        LastMonthSpend = [Math]::Round((Get-CECostAndUsage -TimePeriod $LastMonth     -Granularity Monthly -Metric UNBLENDED_COST).ResultsByTime.Total.Values.Amount,2)
+        Spend          = [Math]::Round((Get-CECostAndUsage -TimePeriod $CurrentPeriod -Granularity Monthly -Metric UNBLENDED_COST).ResultsByTime.Total.Values.Amount,2)
     }
     Try { 
         $Forecast = [Math]::Round(
