61d755911994e028a14801640897b7781c096295
Chad Miles <chadish7@gmail.com>
2021-03-19 15:25:14 -0500

    Replace LaunchTime with RunningTime

--- Functions/Get-CmEc2Instances.ps1
+++ Functions/Get-CmEc2Instances.ps1
@@ -1,17 +1,17 @@
 ﻿Function Get-CmEc2Instances {
     [Cmdletbinding()]
     Param (
-        [string[]]$Region
+        [string[]]$Regions
     )
     $ErrorActionPreference = "Stop"
     $AllRegions = (Get-AWSRegion).Region
-    If (!$Region) {
-        $Region = $AllRegions
+    If (!$Regions) {
+        $Regions = $AllRegions
         Write-Warning "Getting instances for all regions, May take some time"
     } 
-    Foreach ($Reg in $Region) {
-        If ($AllRegions -notcontains $Reg) { Write-Error "$Region is not a valid AWS Region, Valid regions are $AllRegions" }
-        $Instances = (Get-EC2Instance -Region $Reg).Instances
+    Foreach ($Region in $Regions) {
+        If ($AllRegions -notcontains $Region) { Write-Error "$Region is not a valid AWS Region, Valid regions are $AllRegions" }
+        $Instances = (Get-EC2Instance -Region $Region).Instances
         Foreach ($Instance in $Instances) {  
             $Properties = @{
                 Name            = $Instance.Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -ExpandProperty Value
@@ -19,7 +19,11 @@
                 InstanceType    = $Instance.InstanceType
                 InstanceId      = $Instance.InstanceId
                 AZ              = $Instance.Placement.AvailabilityZone
-                LaunchTime      = $Instance.LaunchTime
+                RunningTime     = 
+                If ($Instance.State.Name -eq "Running") {
+                    $ts = New-Timespan -Start $Instance.LaunchTime
+                    ('{0} {1:D2}:{2:D2}:{3:D2}' -f $ts.Days, $ts.Hours, $ts.Minutes, $ts.Seconds).TrimStart("0 :")
+                } Else {}
                 PublicIpAddress = $Instance.PublicIpAddress
             }
             $InstanceObject = New-Object PSObject -Property $Properties
