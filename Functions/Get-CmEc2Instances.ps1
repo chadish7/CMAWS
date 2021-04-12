64e4ac64e2d2aaceb6e3264c9010a795ae946b64
Chad Miles <chadish7@gmail.com>
2021-04-12 16:46:39 -0500

    Optimized Code Added properties

--- Functions/Get-CmEc2Instances.ps1
+++ Functions/Get-CmEc2Instances.ps1
@@ -14,21 +14,20 @@
         If ($AllRegions -notcontains $Region) { Write-Error "$Region is not a valid AWS Region, Valid regions are $AllRegions" }
         $Instances = (Get-EC2Instance -Region $Region).Instances
         Foreach ($Instance in $Instances) {  
-            $Properties = @{
+            [PSCustomObject]@{
                 Name            = $Instance.Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -ExpandProperty Value
                 State           = $Instance.State.Name
                 InstanceType    = $Instance.InstanceType
                 InstanceId      = $Instance.InstanceId
                 AZ              = $Instance.Placement.AvailabilityZone
                 RunningTime     = 
-                If ($Instance.State.Name -eq "Running") {
-                    $ts = New-Timespan -Start $Instance.LaunchTime
-                    ('{0} {1:D2}:{2:D2}:{3:D2}' -f $ts.Days, $ts.Hours, $ts.Minutes, $ts.Seconds).TrimStart("0 :")
-                } Else {}
+                    If ($Instance.State.Name -eq "Running") {
+                        $ts = New-Timespan -Start $Instance.LaunchTime
+                        ('{0} {1:D2}:{2:D2}:{3:D2}' -f $ts.Days, $ts.Hours, $ts.Minutes, $ts.Seconds).TrimStart("0 :")
+                    } Else {}
                 PublicIpAddress = $Instance.PublicIpAddress
+                Platform        = $Instance.Platform.Value 
             }
-            $InstanceObject = New-Object PSObject -Property $Properties
-            Write-Output $InstanceObject
         }
     }
     Write-Output $InstancesList
