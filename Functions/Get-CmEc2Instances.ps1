084194fcbc48617f168caead5a7fec0cf6440dc5
Chad Miles <chadish7@gmail.com>
2024-07-09 10:10:22 -0500

    Formatting

--- Functions/Get-CmEc2Instances.ps1
+++ Functions/Get-CmEc2Instances.ps1
@@ -3,25 +3,29 @@
     [Cmdletbinding()]
     Param (
         [ValidateScript( { @((Get-AWSRegion).Region) })]
-        [string[]]$Regions
+        [string[]] $Regions,
+        [string]   $ProfileName
     )
     $ErrorActionPreference = "Stop"
-    $AllRegions = (Get-AWSRegion).Region
+    $AllRegions = (Get-AWSRegion | Where-Object Region -notlike "*iso*").Region
     If (!$Regions) {
         $Regions = $AllRegions
         Write-Warning "Getting instances for all regions, May take some time"
     } 
-    Foreach ($Region in $Regions) {
+    $Regions | ForEach-Object {
+        $Region = $_
+        $GeneralSplat = @{Region = $Region}
+        if ($ProfileName) { $GeneralSplat.ProfileName = $ProfileName }
         If ($AllRegions -notcontains $Region) { Write-Error "$Region is not a valid AWS Region, Valid regions are $AllRegions" }
-        $Instances = (Get-EC2Instance -Region $Region).Instances
+        $Instances = (Get-EC2Instance @GeneralSplat).Instances
         Foreach ($Instance in $Instances) {  
             [PSCustomObject]@{
-                Name            = $Instance.Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -ExpandProperty Value
-                State           = $Instance.State.Name
-                InstanceType    = $Instance.InstanceType
-                InstanceId      = $Instance.InstanceId
-                AZ              = $Instance.Placement.AvailabilityZone
-                RunningTime     = 
+                Name             = $Instance.Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -ExpandProperty Value
+                State            = $Instance.State.Name
+                InstanceType     = $Instance.InstanceType
+                InstanceId       = $Instance.InstanceId
+                AvailabilityZone = $Instance.Placement.AvailabilityZone
+                RunningTime      = 
                     If ($Instance.State.Name -eq "Running") {
                         $ts = New-Timespan -Start $Instance.LaunchTime
                         ('{0} {1:D2}:{2:D2}:{3:D2}' -f $ts.Days, $ts.Hours, $ts.Minutes, $ts.Seconds).TrimStart("0 :")
@@ -31,5 +35,4 @@
             }
         }
     }
-    Write-Output $InstancesList
 }
