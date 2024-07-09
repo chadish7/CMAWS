5bd70f0cb7cba999479bd9cd47eb53cc9d1c8497
Chad Miles <chadish7@gmail.com>
2024-07-09 08:57:56 -0500

    Updating supported OSes

--- Functions/Get-CmEc2ImageId.ps1
+++ Functions/Get-CmEc2ImageId.ps1
@@ -34,48 +34,49 @@
     The functionality that best describes this cmdlet
 #>
 Function Get-CmEc2ImageId {
-    [OutputType([Amazon.Route53.Model.ChangeInfo])]
+    [OutputType([Amazon.EC2.Model.Image])]
     [CmdletBinding(DefaultParameterSetName = 'Windows')]
     Param(
         [Parameter(Position = 0)]
-        #[ValidatePattern('WindowsServer2(0(1(2R2|6|9)|22)|[12]H2)|Ubuntu(18|2[02])\.04|AmazonLinux2(02\d)?')]
+        #[ValidatePattern('WindowsServer20(16|19|22)|Ubuntu2[0246]\.04|AmazonLinux2(02\d)?')]
         [ValidateSet(
-            "WindowsServer22H2", 
-            "WindowsServer21H2", 
             "WindowsServer2022",
             "WindowsServer2019",
             "WindowsServer2016",
-            "WindowsServer2012R2",
+            "Ubuntu24.04",
             "Ubuntu22.04",
             "Ubuntu20.04",
-            "Ubuntu18.04",
             "AmazonLinux2",
-            "AmazonLinux2022",
             "AmazonLinux2023",
-            "AmazonLinux2025",
-            "AmazonLinux2027",
             "AmazonLinux2NetCore"
         )]
         [string] $OsVersion = "WindowsServer2022",
-        [ValidateSet("2022","2019", "2017", "2016", "2014")]
+        [ValidateSet("2022","2019", "2017", "2016")]
         [Parameter(ParameterSetName='SQL')]
         [string] $SqlVersion,
 
+        # [Windows Only] Return this Edition of SQL for the selected Windows AMI with SQL
         [Parameter(ParameterSetName='SQL')]
         [ValidateSet("Express", "Web", "Standard", "Enterprise")]
         [string] $SqlEdition = "Standard",
 
+        # [Windows Only] Return the Core (No GUI) version of the Windows AMI
         [switch] $Core,
         [Parameter(ParameterSetName='Containers')]
+        # [Windows Only] Return the Containers version of the Windows AMI
         [switch] $Containers,
+        # [Windows Only] Return the ECS Optimized version of the Windows VAMI
         [Parameter(ParameterSetName='Ecs')]
         [switch] $EcsOptimized,
         
         [ValidateScript( { @((Get-AWSRegion).Region) })]
         [string] $Region,
+        
         # The CPU Architecture to use. Available values, arm64 or x86_64.
         [ValidateSet("arm64","x86_64")]
         [string] $Architecture = "x86_64",
+        
+        # [Windows Only] The Language of Windows Server.
         [ValidateSet(
             "Chinese_Traditional", "Chinese_Simplified", "Czech",
             "Dutch", "English", "French", "German", "Hungarian",
@@ -97,17 +98,8 @@ Function Get-CmEc2ImageId {
         $WindowsVersion = $OsVersion.Substring(13)
         if ($Architecture -ne "x86_64") { Write-Error "Windows Server only available on x86_64" }
         $Base         = $True
-        $LatestStable = "22H2", "2022"
-        if ($WindowsVersion -match '2[12]H2') { $Core = $True }
-        if ($Containers) {
-            if ($WindowsVersion -match '2012R2') {
-                Write-Warning "Container AMIs only available for Windows Server 2016 and later, Switching to Windows Server $($LatestStable[1])"
-                $WindowsVersion = $LatestStable[1]
-            }
-        }
         if ($SqlVersion) {
             If ($Core -and $WindowsVersion -ne 2016 -and $SqlVersion ) { Write-Warning "SQL only avaialable on Core Editions of Windows Server 2016, Switching to Full"; $Core = $False }
-            If ($SqlVersion -eq "2014") { $SqlSp = "_SP3" }
             If ($SqlVersion -eq "2016") { $SqlSp = "_SP3" }
             $SqlVersion = $SqlVersion.ToUpper()
             $SqlEdition = $SqlEdition.Substring(0, 1).ToUpper() + $SqlEdition.Substring(1).ToLower()
@@ -116,17 +108,13 @@ Function Get-CmEc2ImageId {
                 Write-Warning "SQL Server $SqlVersion only supported on Windows Server 2016 and 2019, switching to Windows 2019"
                 $WindowsVersion = "2019"
             }
-            if ($WindowsVersion -NE '2022' -and $SqlVersion -EQ '2022') {
-                Write-Warning "SQL Server $SqlVersion only supported on Windows Server 2022, switching to Windows 2022"
+            if ($WindowsVersion -notmatch '20(19|22)' -and $SqlVersion -EQ '2022') {
+                Write-Warning "SQL Server $SqlVersion only supported on Windows Server 2019 and 2022, switching to Windows 2022"
                 $WindowsVersion = "2022"
             }
-            if ($WindowsVersion -notmatch '201(2R2|[69])' -and $SqlVersion -eq "2016") {
+            if ($WindowsVersion -notmatch '201[69])' -and $SqlVersion -eq "2016") {
                 Write-Warning "SQL Server 2016 only supported on Windows Server 2012 R2, 2016 and 2019, switching to Windows 2019"
                 $WindowsVersion = "2019"
-            }
-            if ($WindowsVersion -NE '2012R2' -and $SqlVersion -eq "2014") {
-                Write-Warning "SQL Server 2014 only supported on Windows Server 2012 or 2012 R2, switching to Windows 2012 R2"
-                $WindowsVersion = "2012R2"
             }
         }
         if ($EcsOptimized -or $SqlVersion -or $Containers) { $Base = $False }
@@ -135,11 +123,6 @@ Function Get-CmEc2ImageId {
     
         $BaseText  = "/aws/service/ami-windows-latest/Windows_Server-"
 
-        if ($WindowsVersion -match '2[12]H2') {
-            $SearchString = $BaseText + $WindowsVersion + "-" + $Language + "-Core"
-            if ($Base) { $SearchString += "-Base" }
-            else { $SearchString += "-ContainersLatest" }
-        }
         if ($WindowsVersion -match '20(16|19|22)') {
             if ($Core) { $SearchString = $BaseText + $WindowsVersion + "-" + $Language + "-Core" }
             else { $SearchString = $BaseText + $WindowsVersion + "-" + $Language + "-Full" }
@@ -153,19 +136,19 @@ Function Get-CmEc2ImageId {
                 $SearchString += '-ECS_Optimized/image_id'
             }
         }
-        if ($WindowsVersion -eq "2012R2") {
-            if ($Base) {
-                if ($Core) { $SearchString = $BaseText + "2012-R2_RTM-" + $Language + "-64Bit-Core" }
-                else { $SearchString = $BaseText + "2012-R2_RTM-" + $Language + "-64Bit-Base" }
-            }
-            elseif($SqlVersion) { $SearchString = $BaseText + "2012-R2_RTM-" + $Language + "-64Bit" + $SqlText }
-        }
     }
-    If ($OsVersion -match "^Ubuntu(18|2[02])\.04$") {
+    If ($OsVersion -match "^Ubuntu2\d\.04$") {
+        $UbuntuArch = $Architecture
         if ($Architecture -EQ 'x86_64'){ $UbuntuArch = 'amd64' }
-        else{$UbuntuArch = $Architecture}
-        $Filter = @{Name = "name"; Values = "ubuntu/images/hvm-ssd/ubuntu-*-$($OsVersion.TrimStart("Ubuntu"))-$UbuntuArch-server-20??????" }
-        $Images = Get-Ec2Image @GeneralParams -Filter $Filter
+        $NameFilter = @{
+            Name   = "name"
+            Values = "ubuntu/images/hvm-ssd*/ubuntu-*-$($OsVersion.TrimStart("Ubuntu"))-$UbuntuArch-server-20*"
+        }
+        $OwnerFilter = @{
+            Name   = "owner-alias"
+            Values = "amazon"
+        }
+        $Images = Get-Ec2Image @GeneralParams -Filter $NameFilter, $OwnerFilter
     }
     If ($OsVersion -match "^AmazonLinux2(02\d)?$") {
         $BaseText = "/aws/service/ami-amazon-linux-latest"
