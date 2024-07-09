d6eb8f6aab8d69d9e006b82bed3ed188036419c4
Chad Miles <chadish7@gmail.com>
2024-07-09 10:08:02 -0500

    formatting

--- Functions/Set-CmEc2DnsName.ps1
+++ Functions/Set-CmEc2DnsName.ps1
@@ -1,4 +1,4 @@
-﻿Function Set-CmEc2DnsName            {
+﻿Function Set-CmEc2DnsName {
     Param (
         [Parameter(
             Mandatory=$true,
@@ -14,48 +14,42 @@
         [string]    $InstanceName
         
     )
-    BEGIN 
-    {
-        $ErrorActionPreference      = "Stop"
-    }
-    PROCESS 
-    {
-        foreach ($Instance in $InstanceId)       
-        {
-            $Parameters       = @{InstanceId  = $Instance}
+    BEGIN { $ErrorActionPreference      = "Stop" }
+    PROCESS {
+        foreach ($Instance in $InstanceId) {
+            $Parameters = @{InstanceId = $Instance}
             If ($Region)      { $Parameters.Region      = $Region }
             If ($ProfileName) { $Parameters.ProfileName = $ProfileName }
-            $RunningInstance  = (Get-EC2Instance @Parameters).Instances
+            $RunningInstance = (Get-EC2Instance @Parameters).Instances
             if (!$RunningInstance) {
                 Start-Sleep -Seconds 1
-                $RunningInstance  = (Get-EC2Instance @Parameters).Instances
+                $RunningInstance = (Get-EC2Instance @Parameters).Instances
             }
-            $CurrentState     = $RunningInstance.State.Name.Value
+            $CurrentState = $RunningInstance.State.Name.Value
             if ($CurrentState -like "stop*" -or $CurrentState -like "term*"-or $CurrentState -like "shut*" -or !$CurrentState){
                 Write-Error "Instance $Instance is not running. Please start it or specify another instance"
             }
             If ((Get-Ec2Subnet -SubnetId $RunningInstance.SubnetId -Region $Region).MapPublicIpOnLaunch -eq $true -and !$RunningInstance.PublicIpAddress) {
-                $Counter          = 1
+                $Counter = 1
                 While ($CurrentState -ne "running" -and !$RunningInstance.PublicIpAddress) {
                     Start-Sleep -Seconds 1
                     $Counter++
-                    $RunningInstance  = (Get-EC2Instance @Parameters).Instances
-                    $CurrentState     = $RunningInstance.State.Name.Value
-                    if ($Counter -ge 30)
-                    {
+                    $RunningInstance = (Get-EC2Instance @Parameters).Instances
+                    $CurrentState = $RunningInstance.State.Name.Value
+                    if ($Counter -ge 30){
                         Write-Error "Instance $Instance took too long to start, aborting. WARNING Instance may still start and incur charges."
                     }
                 }
             }
-            If ($InstanceName) {$HostName  = $InstanceName+"."+$DomainName}
-            Else {
+            If ($InstanceName) {
+                $HostName  = $InstanceName+"."+$DomainName
+            } Else {
                 $InstanceName  = ($RunningInstance.Tags | Where-Object {$_.Key -eq "Name"}).Value
-            
+                $HostName  = $InstanceName+"."+$DomainName
                 If (!$InstanceName) {
                     Write-Error "No Name Tag on instance $Instance, can't apply DNS Name."
                     $HostName     = $RunningInstance.PublicDnsName
-                }  Else {
-                    $HostName  = $InstanceName+"."+$DomainName
+                    continue
                 }
             }
             
@@ -71,5 +65,6 @@
             }
         }
     }
-    END{}
+    END {}
 }
+    
